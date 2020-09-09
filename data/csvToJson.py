9ab5cfb1f138c32e8584a5628cc6c93f8be34b58
w0xter <luispozogilo@gmail.com>
2020-09-09 10:52:55 +0200

    Adding BSBM Processed CSVs

--- data/csvToJson.py
+++ data/csvToJson.py
@@ -27,19 +27,16 @@ def transformCSV(file):
     output.close()
 
 def main():
-    path = "./bio2rdf/query2/results/"
-    transformDIR(path)
-    '''
-
+#    path = "./bio2rdf/query2/results/"
+#    transformDIR(path)
     datasets = {
-        'bio2rdf':10,
-        'gtfs':18,
+#        'bio2rdf':10,
+#        'gtfs':18,
         'bsbm':10
     }
     for dataset in datasets.keys():
         for i in range(1,datasets[dataset]):
             path = './' + dataset + '/query' + str(i) + '/results/'
             transformDIR(path)
-    '''
 if __name__ == '__main__':
     main()
